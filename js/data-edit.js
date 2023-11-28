1ea93efd4bd4d761e245e02a15f31f2822051815
Jason Stephen <60260306+JasonStephen@users.noreply.github.com>
2023-11-28 22:33:45 +0800

    Add files via upload

--- js/data-edit.js
+++ js/data-edit.js
@@ -7,9 +7,12 @@ new Vue({
             searchResults: [],
             editingIndex: null,
             newRecord: {},
-            hiddenColumns: ['ID', '隐藏状态', '发现者ID'], //需要隐藏的列名
+            hiddenColumns: ['ID', '发现者ID'], //需要隐藏的列名
             showAdvanced: false,
             sortOptions: [],
+            showDetailsIndex: null, // 用于追踪显示详情的索引
+            sortBy: '发现时间', // 初始排序列
+            sortOrder: 'desc', // 初始排序顺序
         };
     },
     mounted() {
@@ -19,8 +22,10 @@ new Vue({
         // // 清空排序选项数组，确保每次都是初始状态
         // this.sortOptions = [];
 
-        // 在默认排序下按照 ID 降序排序
-        this.toggleSort('ID');
+        // 初次加载时按照“发现时间”的列降序排序
+        this.toggleSort('发现时间');
+
+        
     },
     methods: {
         getCsvData() { //返回CSV数据
@@ -37,20 +42,27 @@ new Vue({
                 });
         },
 
-        parseCsvData(data) { //在这个地方添加需要转换的字符类型
-            // 初始化 CSV 数据并转换 ID 列为整数类型
+        parseCsvData(data) {
+            console.log('[COMPLETED] Parse CSV data.');
+            // 初始化 CSV 数据并转换 ID 和浏览量 列为整数类型
             return data.map(row => {
                 // 使用解构赋值创建新的对象以避免直接修改原始数据
                 const newRow = { ...row };
-
+        
                 // 将 ID 列转换为整数类型
                 if (newRow.hasOwnProperty('ID')) {
                     newRow.ID = parseInt(newRow.ID, 10);
                 }
-
+        
+                // 将浏览量列转换为整数类型，假设浏览量列为 '浏览量'
+                if (newRow.hasOwnProperty('浏览量')) {
+                    newRow.浏览量 = parseInt(newRow.浏览量, 10);
+                }
+        
                 return newRow;
             });
         },
+        
 
         toggleAdvanced() {
             console.log('[COMPLETED] Toggle advanced options.');
@@ -59,62 +71,60 @@ new Vue({
 
         toggleSort(column) {
             console.log('[COMPLETED] Toggle sort.');
-            const index = this.sortOptions.findIndex(opt => opt.column === column);
-        
-            if (index === -1) {
-                // 如果没有排序选项，直接指向当前列并添加降序排序
-                this.sortOptions = [{ column, order: 'desc' }];
-                console.log(`实现${column}降序1`);
-            } else if (this.sortOptions[index].order === 'desc') {
-                // 如果当前是降序，切换为升序
-                this.sortOptions[index].order = 'asc';
-                console.log(`实现${column}升序2`);
-            } else {
-                // 如果当前是升序，或是默认排序，切换为仅按照 ID 列降序排序
-                this.sortOptions = [{ column: 'ID', order: 'desc' }];
-                console.log('仅按照 ID 列降序排序3');
+            // 切换排序列
+            if (this.sortBy === column) {
+                // 如果点击的是当前排序列，不需要切换排序顺序，直接返回
+                return;
             }
-        
+
+            // 如果点击的是新的列，切换到升序
+            this.sortBy = column;
+            this.sortOrder = 'desc';
+
             // 更新排序后的结果
             this.sortResults();
-        },        
-    
+
+            // 关闭对应的窗口
+            this.closeDetails();
+            this.cancelEdit();
+        },
+
         getSortIndicator(column) {
             console.log('[COMPLETED] Get sort indicator.');
-            const sortOption = this.sortOptions.find(opt => opt.column === column);
-    
-            if (sortOption) {
-                return sortOption.order === 'desc' ? '降序' : '升序';
+            // 根据当前排序状态返回排序指示符
+            if (this.sortBy === column) {
+                return this.sortOrder === 'desc' ? '降序' : '升序';
             }
-    
             return '';
         },
-    
+
         sortResults() {
             console.log('[COMPLETED] Sort results.');
             this.searchResults.sort((a, b) => {
-                for (const { column, order } of this.sortOptions) {
-                    const aValue = a[column];
-                    const bValue = b[column];
-    
-                    // 处理 undefined 或 null 值
-                    if (aValue === undefined || aValue === null) {
-                        return order === 'desc' ? 1 : -1;
-                    }
-                    if (bValue === undefined || bValue === null) {
-                        return order === 'desc' ? -1 : 1;
-                    }
-    
-                    // 根据类型进行比较
-                    const result = typeof aValue === 'string' && typeof bValue === 'string'
-                        ? aValue.localeCompare(bValue)
-                        : aValue < bValue ? -1 : (aValue > bValue ? 1 : 0);
-    
-                    // 根据排序顺序返回结果
-                    return order === 'desc' ? -result : result;
+                const aValue = a[this.sortBy];
+                const bValue = b[this.sortBy];
+        
+                // 处理 undefined 或 null 值
+                if (aValue === undefined || aValue === null) {
+                    return this.sortOrder === 'desc' ? 1 : -1;
                 }
-                return 0;
+                if (bValue === undefined || bValue === null) {
+                    return this.sortOrder === 'desc' ? -1 : 1;
+                }
+        
+                // 根据类型进行比较
+                const result = typeof aValue === 'string' && typeof bValue === 'string'
+                    ? aValue.localeCompare(bValue)
+                    : aValue < bValue ? -1 : (aValue > bValue ? 1 : 0);
+        
+                // 根据排序顺序返回结果
+                return this.sortOrder === 'desc' ? -result : result;
             });
+        },
+        
+
+        getSortButtonColor(column) {
+            return this.sortBy === column ? 'blue' : 'black';
         },
 
         search() {
@@ -141,11 +151,6 @@ new Vue({
             this.sortResults();
         },
 
-        editRow(index) { //编辑行
-            console.log('[COMPLETED] Edit row.');
-            this.editingIndex = index;
-        },
-
         addRow() {
             console.log('[COMPLETED] Add row.');
             // 如果正在编辑其他行，先保存编辑
@@ -156,6 +161,12 @@ new Vue({
         
             // 在添加行之前，你可以初始化新行的数据，例如：
             this.newRecord = {};
+
+            // 设置"发现时间"列的值为今天的年月日
+            const today = new Date();
+            const year = today.getFullYear();
+            const month = String(today.getMonth() + 1).padStart(2, '0');
+            const day = String(today.getDate()).padStart(2, '0');
         
             // 获取 CSV 文件的列名
             const columns = Object.keys(this.csvData[0]);
@@ -167,7 +178,8 @@ new Vue({
                     if (column === 'ID') {
                         const lastId = this.csvData.length > 0 ? this.csvData[this.csvData.length - 1].ID : 0;
                         this.newRecord[column] = lastId + 1;
-                        console.log('[AddRow] run newRecord[column] = lastId + 1;');
+                    } else if (column === '发现时间') {
+                        this.newRecord[column] = `${year}-${month}-${day}`;
                     } else if (column === '隐藏状态') {
                         this.newRecord[column] = '0';
                     } else {
@@ -187,56 +199,84 @@ new Vue({
             // 重新执行搜索，确保新添加的行也能被显示
             this.search();
         
-            // 调用EditRow去编辑当前添加的行
-            this.editRow(this.editingIndex);
+            // 调用EditDetails去编辑当前添加的行
+            this.editDetails(this.editingIndex);
+
+            this.closeDetails();
         },
         
+        editDetails(index) {
+            console.log('[COMPLETED] Edit details.');
+            // 设置编辑状态为当前详情信息的索引
+            this.editingIndex = index;
 
-        changeHiddenState(index) {
-            console.log('[COMPLETED] Change hidden state.');
-        
-            // 直接从 csvData 数组中移除相应的行
-            this.csvData.splice(index, 1);
-        
+            // 关闭详情信息
+            this.closeDetails();
+        },
+    
+        deleteDetails(index) {
+            console.log('[COMPLETED] Delete details.');
+            // 在 searchResults 数组中找到相应的数据
+            const itemToDelete = this.searchResults[index];
+            // 在 csvData 数组中找到该数据的索引
+            const indexInCsvData = this.csvData.findIndex(item => item === itemToDelete);
+            // 在 csvData 数组中移除相应的数据
+            if (indexInCsvData !== -1) {
+                this.csvData.splice(indexInCsvData, 1);
+            }
             // 如果需要，可以将更改后的数据保存到服务器
             this.saveToServer();
-        
-            // 清除编辑状态
+            // 清除编辑状态和详情视图
             this.editingIndex = null;
-        
-            // 重新执行搜索，确保新添加的行也能被显示
-            this.search();
         },
-        
-        saveEdit() { //保存编辑
+
+        saveEdit() {
             console.log('[COMPLETED] Save edit.');
             // 执行任何必要的验证或保存逻辑
             this.editingIndex = null;
-
+    
             // 发送更新后的数据到服务器保存
             this.saveToServer();
-
+    
             // 如果编辑在最后一行，添加新行
             if (this.editingIndex === this.csvData.length - 1) {
                 this.addRow();
             }
-
+    
             this.search();
-
+    
             console.log('Edited Row:', this.searchResults[this.editingIndex]);
             console.log('CSV Data:', this.csvData);
+        },
+
+        viewDetails(index) {
+            console.log('[COMPLETED] View details.');
+            // 设置 showDetailsIndex 为当前点击的行的索引
+            this.showDetailsIndex = index;
+            
+            // 在这里增加对浏览量字段的加一操作
+            if (this.searchResults[index].hasOwnProperty('浏览量')) {
+                this.searchResults[index]['浏览量'] = (this.searchResults[index]['浏览量'] || 0) + 1;
+        
+                // 如果需要，你也可以将更新后的数据保存到服务器
+                this.saveToServer();
+            }
+            this.cancelEdit();
+        },
+        
+        closeDetails() {
+            console.log('[COMPLETED] Close details.');
+            // 关闭详情视图，将 showDetailsIndex 设置为 null
+            this.showDetailsIndex = null;
         },
 
         cancelEdit() {
             console.log('[COMPLETED] Cancel edit.');
             // 取消编辑，恢复原始数据
-            if (this.editingIndex !== null) {
-                // 如果有正在编辑的行，将编辑状态设置为null
                 this.editingIndex = null;
     
                 // 如果有修改过的数据，恢复为原始数据
                 this.searchResults[this.editingIndex] = { ...this.csvData[this.editingIndex] };
-            }
         },
         
         saveToServer() { //发送更新后的 CSV 数据到服务器
--- js/user-login-reg.js
+++ js/user-login-reg.js
@@ -1,78 +1,137 @@
 // path/to/your/script.js
 
 new Vue({
-    el: '#app',
-    data: {
-        username: '',
-        password: ''
-    },
-    methods: {
-        login() {
-            // Make a POST request to your server's login endpoint
-            fetch('http://localhost:3000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: this.username,
-                    password: this.password
-                }),
-            })
-            .then(response => {
-                if (response.ok) {
-                    console.log('User logged in successfully.');
-                    // Redirect or perform any action upon successful login
-                } else if (response.status === 401) {
-                    alert('Invalid username or password');
-                } else {
-                    alert('An error occurred. Please try again.');
-                }
-            })
-            .catch(error => {
-                console.error('Error during login:', error);
-                alert('An error occurred. Please try again.');
-            });
-        }
-    }
-});
-
-
-
-new Vue({
-    el: '#registration',
-    data: {
+  el: '#app',
+  data: {
+      username: '',
+      password: '',
       newUser: {
-        username: '',
-        password: '',
-        email: '',
-        role: ''
-      }
-    },
-    methods: {
+          username: '',
+          password: '',
+          email: '',
+          role: ''
+      },
+      csvData: [] // You need to set or fetch csvData
+  },
+  mounted() {
+      // Fetch csvData or set it in some way
+      // For example, you can fetch it from the server or set it from another source
+      this.csvData = [
+          // ... your CSV data
+      ];
+  },
+  methods: {
+      login() {
+          // Make a POST request to your server's login endpoint
+          fetch('http://localhost:3000/login', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  username: this.username,
+                  password: this.password
+              }),
+          })
+          .then(response => {
+              if (response.ok) {
+                  console.log('User logged in successfully.');
+                  // Redirect or perform any action upon successful login
+              } else if (response.status === 401) {
+                  alert('Invalid username or password');
+              } else {
+                  alert('An error occurred. Please try again.');
+              }
+          })
+          .catch(error => {
+              console.error('Error during login:', error);
+              alert('An error occurred. Please try again.');
+          });
+      },
       register() {
-        // Make a POST request to your server's register endpoint
+        // 获取用户数据
+        const newUser = {
+            username: this.newUser.username,
+            password: this.newUser.password,
+            email: this.newUser.email,
+            role: this.newUser.role
+        };
+
+        // 发送注册请求
         fetch('http://localhost:3000/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(this.newUser),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUser),
         })
         .then(response => {
-          if (response.ok) {
-            console.log('User registered successfully.');
-            // Redirect or perform any action upon successful registration
-          } else if (response.status === 400) {
-            alert('User already exists');
-          } else {
-            alert('An error occurred. Please try again.');
-          }
+            if (response.ok) {
+                console.log('User registered successfully.');
+
+                // 更新本地 CSV 文件
+                this.updateLocalCSVFile(() => {
+                    // 清空注册表单
+                    this.newUser = {
+                        username: '',
+                        password: '',
+                        email: '',
+                        role: ''
+                    };
+                });
+            } else if (response.status === 400) {
+                alert('User already exists');
+            } else {
+                alert('An error occurred. Please try again.');
+            }
         })
         .catch(error => {
-          console.error('Error during registration:', error);
-          alert('An error occurred. Please try again.');
+            console.error('Error during registration:', error);
+            alert('An error occurred. Please try again.');
         });
-      }
+    },
+
+    // 新增的方法，用于更新本地 CSV 文件
+    updateLocalCSVFile(callback) {
+        // 获取当前 CSV 数据
+        fetch('http://localhost:3000/get-csv')
+            .then(response => response.json())
+            .then(csvData => {
+                // 添加新用户的数据到 CSV 数据中
+                csvData.push({
+                    Username: this.newUser.username,
+                    ID: csvData.length > 0 ? csvData[csvData.length - 1].ID + 1 : 1,
+                    Email: this.newUser.email,
+                    Password: this.newUser.password,
+                    Role: this.newUser.role
+                });
+
+                // 发送更新 CSV 数据请求
+                fetch('http://localhost:3000/save-csv', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(csvData),
+                })
+                .then(response => {
+                    if (response.ok) {
+                        console.log('CSV file updated successfully.');
+                        callback(); // 执行回调函数
+                    } else {
+                        console.error('Error updating CSV file.');
+                        alert('An error occurred. Please try again.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error updating CSV file:', error);
+                    alert('An error occurred. Please try again.');
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching CSV data:', error);
+                alert('An error occurred. Please try again.');
+            });
     }
-  });
+  }
+});
